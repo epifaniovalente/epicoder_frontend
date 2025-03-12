38290dfef396e244b52c6755c9020b5633d68e34
EpiCoder_# <epifaniovalente1@gmail.com>
2025-03-12 13:28:43 +0100

    Melhorando o Coidgo
    EpiCoder

--- Loja Virtual/src/JS/script.js
+++ Loja Virtual/src/JS/script.js
@@ -6,30 +6,36 @@ menuBtn.onclick = function () {
     const mostrar = menuDrop.classList.contains('abrir')
 }
 
-let adicionados = 0;
- document.getElementById("btn_add").addEventListener("click", ()=>{
-     adicionados ++;
-    document.getElementById("nitem").textContent = adicionados
- })
-
-
 //Numero de Itens
-
 function itens() {
     const itens = document.querySelectorAll("section .produto").length;
-    document.getElementById("itens").textContent = itens  
+    document.getElementById("itens").textContent = itens
 }
 
 itens();
 
-//Notificação Temporaria & ADD ao Carrinho
-const notificar = document.getElementById('btn_add').addEventListener("click", ()=> {
-    const sms = document.getElementById('sms')
-    sms.style.display = "block";
+/*Notificação Temporaria & ADD ao Carrinho
+//const notificar = document.getElementById('btn_add').addEventListener("click", () => {
+
+})
+    Produtos adicionados no carrinho
+let adicionados = 0;
+document.getElementById("btn_add").addEventListener("click", () => {
+    adicionados++;
+    document.getElementById("nitem").textContent = adicionados
+})
+    */
+
+let add = 0;
+function notificar() {
+    let sms = document.getElementById('sms');
+    sms.style.display = 'block'
+    sms.style.opacity = '1'
 
     setTimeout(() => {
-        sms.style.opacity = "0"
-    }, 3500);
-})
+        sms.style.opacity = '0'
+    }, 3000);
 
-
+    add++;
+    document.getElementById('nitem').textContent = add;
+}
