c8aeb63afbdd9fead67395c9dca084372ce7a816
EpiCoder_# <epifaniovalente1@gmail.com>
2025-06-19 00:36:30 +0100

    EpiCoder
    Mexendo no Background

---
+++ Denúncia Anônima/JS/script.js
@@ -0,0 +1,4 @@
+let menu = document.getElementById("menu_drop").addEventListener("click",function () {
+    //window.alert("ola")
+    document.getElementById("links_drop").classList.toggle("abrir");
+});
