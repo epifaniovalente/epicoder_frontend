08975e840212efe577a2a646d404315210e08c74
EpiCoder_# <epifaniovalente1@gmail.com>
2025-07-09 20:52:12 +0100

    portifolio
    menu alterado

--- epicoder/JS/script.js
+++ epicoder/JS/script.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
+const btn = document.getElementById('botao');
+const menu = document.querySelector('.menu-link');
 
-});
+btn.addEventListener('click',function () {
+    menu.classList.toggle('abrir');
+})
