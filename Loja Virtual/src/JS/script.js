22c98a7b06972bccb9c6b907c1ec11c91ba8724a
EpiCoder_# <epifaniovalente1@gmail.com>
2025-07-18 12:49:14 +0100

    EpiCoder
    Tela de cadastro

--- Loja Virtual/src/JS/script.js
+++ Loja Virtual/src/JS/script.js
@@ -1,3 +1,52 @@
+/*Validação do Formulario */
+
+function Verificar(variavel) {
+    if (variavel.length < 1) return false;
+    return true
+}
+
+const formulario = document.getElementById('login');
+
+formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const errorEmail = document.getElementById('errorEmail');
+    const errorSenha = document.getElementById('errorSenha');
+
+
+    if (!Verificar(email)) {
+        errorEmail.textContent = 'Este campo é obrigatorio!';
+    } else {
+        errorEmail.textContent = '';
+    }
+
+    if (!Verificar(senha)) {
+        errorSenha.textContent = 'Este campo é obrigatorio!'
+    } else {
+        errorSenha.textContent = ''
+    }
+
+    /*
+    function ValidarEmail(variavel) {
+        return variavel.includes('@') && 
+        variavel.indexOf('.') > variavel.indexOf('@') + 1 && variavel.length > 12;
+    }
+    function ValidarSenha(variavel) {
+        if (variavel.length < 0) return false;
+        if (!/[A-Z]/.test(variavel)) return false;
+        if (!/[a-z]/.test(variavel)) return false;
+        if (!/[0-9]/.test(variavel)) return false;
+        if (!/[\W_]/.test(variavel)) return false;
+        return true;
+    }
+    */
+})
+
+/*FIM VALIDAÇÃO */
+
+
 const menuBtn = document.querySelector('.menu');
 const menuDrop = document.querySelector('.menu_dropdown');
 
