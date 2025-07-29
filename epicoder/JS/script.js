3cbe43a612161f23fa7be035163b867e201c680e
EpiCoder_# <epifaniovalente1@gmail.com>
2025-07-30 00:21:20 +0100

    EpiCoder - terminando seções

--- epicoder/JS/script.js
+++ epicoder/JS/script.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu-link');
 btn.addEventListener('click',function () {
     menu.classList.toggle('abrir');
 })
+
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click',function(){
+        menu.classList.remove('abrir')
+    })
+});
