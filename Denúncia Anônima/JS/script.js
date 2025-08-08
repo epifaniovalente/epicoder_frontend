4f77c05437d7be56fc76e3984f13f201c9b3d991
EpiCoder_# <epifaniovalente1@gmail.com>
2025-08-08 10:06:57 +0100

    EpiCoder
    Outras abas

--- Denúncia Anônima/JS/script.js
+++ Denúncia Anônima/JS/script.js
@@ -31,23 +31,75 @@ login_btn.addEventListener("click", function (event) {
   let error_senha = document.getElementById("error_senha");
 
   if (verificar(email)) {
-    error_email.textContent = 'Preencha corretamente!';
-  }else{
-    error_email.textContent = '';
+    error_email.textContent = "Preencha corretamente!";
+  } else {
+    error_email.textContent = "";
   }
 
   if (verificar(senha)) {
-    error_senha.textContent = 'Preencha corretamente!';
+    error_senha.textContent = "Preencha corretamente!";
   } else {
-    error_senha.textContent = '';
+    error_senha.textContent = "";
   }
 });
 
-
 /*SCRIPT PAGINA DENUNCIAR */
 
-const send_btn = document.getElementById('send-btn');
+function select_Validator(variavel) {
+  return variavel === "" || variavel === "0";
+}
 
-send_btn.addEventListener('click',function (event) {
+function inputLocal_Validator(variavel) {
+  return variavel.length < 5;
+}
+
+function inputDesc_Validator(variavel) {
+  if (variavel.length < 50) return false;
+  return true;
+}
+
+function validator(event) {
   event.preventDefault();
-})
+
+  /*Valores dos inputs */
+  const tipo_dn = document.getElementById("tipo-denuncia");
+  const loca_dn = document.getElementById("local-incidente");
+  const desc_dn = document.getElementById("descricao");
+
+  /*Variaveis de erro */
+  const error_tipo = document.getElementById("error-tipo");
+  const error_local = document.getElementById("error-local");
+  const error_desc = document.getElementById("error-desc");
+
+  /*Validar */
+
+  /*LOGICA PARA VALIDAR Input Select */
+  // if (tipo_dn.value === "" || tipo_dn.value === "0") {
+  //   tipo_dn.classList.add('element-error')
+  // } else {
+
+  // }
+  if (select_Validator(tipo_dn.value)) {
+    tipo_dn.classList.add("element-error");
+    error_tipo.textContent = "Este campo é obrigatório! *";
+  } else {
+    tipo_dn.classList.remove("element-error");
+    error_tipo.textContent = "";
+  }
+
+  if (inputLocal_Validator(loca_dn.value)) {
+    loca_dn.classList.add("element-error");
+    error_local.textContent = "Este campo é obrigatório! *";
+  } else {
+    loca_dn.classList.remove("element-error");
+    error_local.textContent = "";
+  }
+
+  if (!inputDesc_Validator(desc_dn.value)) {
+    desc_dn.classList.add("element-error");
+    error_desc.textContent = "Este campo é obrigatório! *";
+  } else {
+    desc_dn.classList.remove("element-error");
+    error_desc.textContent = "";
+  }
+}
