3cedc7550beb85e38f7f0b6c382633c6a7def063
EpiCoder_# <epifaniovalente1@gmail.com>
2025-09-13 16:47:08 +0100

    EpiCoder
    push pendente "don't remember"

--- Portifólio/src/JS/code.js
+++
@@ -1,6 +0,0 @@
-const btn_menu = document.getElementById('menu-drop');
-const lista = document.getElementById('drop-list')
-
-btn_menu.addEventListener('click', function () {
-  lista.classList.toggle('abrir')
-});
